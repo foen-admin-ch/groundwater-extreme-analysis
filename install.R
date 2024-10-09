23e8400f1044064d5b80cd7017d647a2154ee9e1
R. F. <rdefondeville@gmail.com>
2024-10-09 14:12:17 +0000

    Update install.R

--- install.R
+++ install.R
@@ -6,4 +6,16 @@
 # For example, remove the comment from the line below if you wish to
 # install the `ggplot2` package.
 #
-# install.packages('ggplot2')
+
+# For easy date manipulation
+install.packages('lubridate')
+
+#To fit Extreme Value Distributions using maximum likelihood
+install.packages('evd')
+
+# To fir parametric models using l-moment matching
+install.packages('lmom')
+
+# To estimate automatically trends from data
+install.packages('mgcv')
+
