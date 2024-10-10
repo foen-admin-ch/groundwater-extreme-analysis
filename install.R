1fb5bd342b0585c567af9f5a6ead4fb7dea19b4b
R. F <rdefondeville@gmail.com>
2024-10-10 08:07:02 +0000

    start example 4 notebook

--- install.R
+++ install.R
@@ -13,7 +13,7 @@ install.packages('lubridate')
 #To fit Extreme Value Distributions using maximum likelihood
 install.packages('evd')
 
-# To fir parametric models using l-moment matching
+# To fit parametric models using l-moment matching
 install.packages('lmom')
 
 # To estimate automatically trends from data
