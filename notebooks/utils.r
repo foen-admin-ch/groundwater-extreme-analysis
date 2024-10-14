c89362465d77c88d77dd4df5d4588ff08f09e114
R. F <rdefondeville@gmail.com>
2024-10-14 13:09:21 +0000

    update bootstrap procedure

--- notebooks/utils.r
+++ notebooks/utils.r
@@ -18,12 +18,12 @@ display_return_level_function <- function(data, parameters, max_trend = 0, max_r
     data_maxima_sorted$year <- 1 / (1 - 1:n / (n + 1)) * block_size
     
     # Compute retrun level function
-    return_levels <- compute_return_level_function(model = model, parameters = parameters, max_trend = max_trend, max_return_period = max(2  * n,max_return_period))
+    return_levels <- compute_return_level_function(model = model, parameters = parameters, n_obs = n, max_trend = max_trend, max_return_period = max(2  * n,max_return_period))
     
     # Create return level function plot
     p <- ggplot() +
         geom_line(data = return_levels$return_period_estimates, aes(x = year, y = return_levels)) + 
-        geom_line(data = return_levels$model_uncertainty, aes(x = year, y = lower), linetype = "dotted", color = "black") + 
+        geom_line(data = return_levels$model_uncertainty, aes(x = year, y = lower), linetype = "dotted", color = "black") +  
         geom_line(data = return_levels$model_uncertainty, aes(x = year, y = upper), linetype = "dotted", color = "black") + 
         geom_point(data = data_maxima_sorted, aes(x = year, y = Max)) +
         scale_x_continuous(trans='log2', breaks = c(1,2,5,10,20, 50 ,100)) +
@@ -34,7 +34,7 @@ display_return_level_function <- function(data, parameters, max_trend = 0, max_r
 }
 
 # Function to povide a return level and associated uncertainy for a given return period
-compute_return_level <- function(return_period, parameters, max_trend = 0, max_year = 100){
+compute_return_level <- function(return_period, parameters, n_obs, max_trend = 0){
     # Determine model and estimation method
     parameters_name <- names(parameters)
 
@@ -48,8 +48,7 @@ compute_return_level <- function(return_period, parameters, max_trend = 0, max_y
     print(model)
     
     
-    # Compute return level
-    #return_level <- qweibull(p = 1 - 1 / (return_period / block_size), scale = wei[2], shape = wei[3]) + as.numeric(wei[1]) + max_trend
+    # Compute return level from estimates
     if(model == "GEV_LMOM"){
     return_level <-  lmom::quagev(1 - 1 / (return_period / block_size), para = parameters) + max_trend
         }
@@ -64,30 +63,50 @@ compute_return_level <- function(return_period, parameters, max_trend = 0, max_y
         }
     
         
-        
-    # Compute retrun level function to obtain uncertainty quantification through function interporlation
-    return_level_function <- compute_return_level_function(model = model, parameters = parameters, max_trend = max_trend, max_return_period = 2 * return_period)
-    
-    # Find closest point on curve
-    pos1 <- which(rank(abs((return_level_function$model_uncertainty)$year - return_period)) == 1)
-    # Find second closest point on curve
-    pos2 <- which(rank(abs((return_level_function$model_uncertainty)$year - return_period)) == 2)
-    # Compute linear weights for interpollation
-    weights <- c(1 / abs(return_level_function$model_uncertainty$year - 10)[c(pos1,pos2)]) / sum(1 / abs(return_level_function$model_uncertainty$year - 10)[c(pos1,pos2)]) 
-   
-    # Compute interpolated confidence bounds
-    uncertainty_interval <- c(sum(return_level_function$model_uncertainty[c(pos1,pos2),1] * weights), sum(return_level_function$model_uncertainty[c(pos1,pos2),2] * weights))
+        # Number of bootstrap samples
+    m <- 1000
+    # Bootstrap matrix
+    uncertainty_param_bootstrap <- rep(NA,m)
+                                          
+    #Generate m samples of size n
+    if(model == "GEV_LMOM"){
+            for(i in 1:m){
+            lmom <- samlmu(lmom::quagev(runif(n_obs), parameters))
+            gev <- pelgev(lmom)
+            uncertainty_param_bootstrap[i] <- lmom::quagev(1 - 1 / (return_period / block_size), para = gev) + max_trend
+            }
+    }
+    if(model == "Weibull"){
+        for(i in 1:m){
+        lmom <- samlmu(qweibull(p = runif(n_obs), scale = parameters[2], shape = parameters[3]) + parameters[1])
+            try(expr = {wei <- pelwei(lmom)
+        uncertainty_param_bootstrap[i] <- qweibull(p = 1 - 1 / (return_period / block_size), scale = wei[2], shape = wei[3]) + wei[1] + max_trend}, silent = TRUE)
+            }
+    }
+    if(model == "P3"){
+        for(i in 1:m){
+            lmom <- samlmu(lmom::quape3(runif(n_obs), parameters))
+            p3 <- pelpe3(lmom)
+            uncertainty_param_bootstrap[i] <- lmom::quape3(1 - 1 / (return_period / block_size), para = p3) + max_trend
+            }
+    }
+    if(model == "GEV_MLE"){
+        for(i in 1:m){
+            gev_fit <- evd::fgev(evd::qgev(p = runif(n_obs), shape = parameters["shape"], loc = parameters["loc"], scale = parameters["scale"]), std.err = FALSE)
+           uncertainty_param_bootstrap[i] <- evd::qgev(p = 1 - 1 / (return_period / block_size), shape = gev_fit$param["shape"], loc = gev_fit$param["loc"], scale = gev_fit$param["scale"])  + max_trend
+            }
+    }
     
     #Return result as dataframe
     return(data.frame(Return_period = return_period,
                      Return_level = return_level,
-                     Lower_CI_bound = uncertainty_interval[1],
-                     Upper_CI_bound = uncertainty_interval[2]))
+                     Lower_CI_bound = quantile(uncertainty_param_bootstrap, p = 0.005, na.rm = TRUE),
+                     Upper_CI_bound = quantile(uncertainty_param_bootstrap, p = 0.995, na.rm = TRUE)))
 }
 
 
 # Function to compute the return level function and associated uncertainty
-compute_return_level_function <- function(model, parameters, max_trend = 0, max_return_period = 99){
+compute_return_level_function <- function(model, parameters, n_obs, max_trend = 0, max_return_period = 99){
     #Compute return level as function of the model and the estimated parameters
     return_period_estimates <- data.frame(year = c(seq(1.01,1.9, by = 0.01) , 2:9, 1:9 * 10, max(100,max_return_period)) * block_size) 
     
@@ -104,36 +123,44 @@ compute_return_level_function <- function(model, parameters, max_trend = 0, max_
     return_period_estimates$return_levels <- evd::qgev(p = 1 - 1 / (return_period_estimates$year / block_size), shape = parameters["shape"], loc = parameters["loc"], scale = parameters["scale"]) + max_trend
         }
     
-    # Size of bootstrap samples
-    n <- max(100, max_return_period)
-    pobs <- (1:n)/(n + 1)
     # Number of bootstrap samples
-    m <- 10000
-    
+    m <- 1000
+    # Bootstrap matrix
+    uncertainty_param_bootstrap <- matrix(NA,m,length(return_period_estimates$year))
+                                          
     #Generate m samples of size n
     if(model == "GEV_LMOM"){
-    uncertainty_param_bootstrap <- matrix(lmom::quagev(runif(m*n), parameters), m,n)
+            for(i in 1:m){
+            lmom <- samlmu(lmom::quagev(runif(n_obs), parameters))
+            gev <- pelgev(lmom)
+            uncertainty_param_bootstrap[i,] <- lmom::quagev(1 - 1 / (return_period_estimates$year / block_size), para = gev) + max_trend
+            }
     }
     if(model == "Weibull"){
-    uncertainty_param_bootstrap <- matrix(qweibull(p = runif(n*m), scale = parameters[2], shape = parameters[3]) + parameters[1], m,n)
+        for(i in 1:m){
+        lmom <- samlmu(qweibull(p = runif(n_obs), scale = parameters[2], shape = parameters[3]) + parameters[1])
+            try(expr = {wei <- pelwei(lmom)
+        uncertainty_param_bootstrap[i,] <- qweibull(p = 1 - 1 / (return_period_estimates$year / block_size), scale = wei[2], shape = wei[3]) + wei[1] + max_trend}, silent = TRUE)
+            }
     }
     if(model == "P3"){
-    uncertainty_param_bootstrap <- matrix(lmom::quape3(runif(m*n), parameters), m,n)
+        for(i in 1:m){
+            lmom <- samlmu(lmom::quape3(runif(n_obs), parameters))
+            p3 <- pelpe3(lmom)
+            uncertainty_param_bootstrap[i,] <- lmom::quape3(1 - 1 / (return_period_estimates$year / block_size), para = p3) + max_trend
+            }
     }
     if(model == "GEV_MLE"){
-    uncertainty_param_bootstrap <- matrix(evd::qgev(p = runif(m*n), shape = parameters["shape"], loc = parameters["loc"], scale = parameters["scale"]), m,n)
-    }
-    
-    
-    # Sort samples for quantile computation
-    for(i in 1:nrow(uncertainty_param_bootstrap)){
-        uncertainty_param_bootstrap[i,] <- sort(uncertainty_param_bootstrap[i,])
+        for(i in 1:m){
+            gev_fit <- evd::fgev(evd::qgev(p = runif(n_obs), shape = parameters["shape"], loc = parameters["loc"], scale = parameters["scale"]), std.err = FALSE)
+           uncertainty_param_bootstrap[i,] <- evd::qgev(p = 1 - 1 / (return_period_estimates$year / block_size), shape = gev_fit$param["shape"], loc = gev_fit$param["loc"], scale = gev_fit$param["scale"])
+            }
     }
 
-    #Store bottstrap confidence intervals into dataframe
-    model_uncertainty <- data.frame(lower = apply(uncertainty_param_bootstrap, 2, quantile, p = 0.005),
-                              upper = apply(uncertainty_param_bootstrap, 2, quantile, p = 0.995)) + max_trend
-    model_uncertainty$year <- (1 / (1 - pobs)) * block_size
+    #Store bootstrap confidence intervals into dataframe
+    model_uncertainty <- data.frame(lower = apply(uncertainty_param_bootstrap, 2, quantile, p = 0.005, na.rm = TRUE),
+                              upper = apply(uncertainty_param_bootstrap, 2, quantile, p = 0.995,  na.rm = TRUE))
+    model_uncertainty$year <- return_period_estimates$year
     
     return(list(return_period_estimates = return_period_estimates,
                model_uncertainty = model_uncertainty))
